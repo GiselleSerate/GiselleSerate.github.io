34168e9525eb7cf44a3ed22d608b0ba6c2be4b21
GiselleSerate <giselle.serate@gmail.com>
2019-07-04 11:17:22 -0700

    Change background and transparency the background

--- src/js/app.js
+++ src/js/app.js
@@ -19,6 +19,4 @@ $(document).ready(function() {
     }
   });
 
-  // Animate on scroll. 
-  // AOS.init();
 });
--- src/webpack.config.js
+++ src/webpack.config.js
@@ -33,6 +33,10 @@ module.exports = {
       {
         test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
         loader: "file-loader"
+      },
+      {
+        test: /\.html/,
+        loader: 'html-loader'
       }
     ]
   }
